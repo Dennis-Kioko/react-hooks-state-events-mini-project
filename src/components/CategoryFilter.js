503ff38e73345e3b294fec85c2411a018a639c2f
denniskioko <kiokodennis6990@gmail.com>
2024-02-05 11:51:43 +0300

    Completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, tasks, setFilteredTasks }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleFiteredTasks(category) {
+    const filteredTasks =
+      category === "All"
+        ? tasks
+        : tasks.filter((task) => task.category === category);
+    setFilteredTasks(filteredTasks);
+    setSelectedCategory(category);
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {
+        /* render <button> elements for each category here */
+        categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => handleFiteredTasks(category)}
+            className={`category ${
+              category === selectedCategory ? "selected" : ""
+            }`}
+          >
+            {category}
+          </button>
+        ))
+      }
     </div>
   );
 }
